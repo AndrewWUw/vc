adfa8f10c74e0d2cecc69c5c095ee18609f5aa26
AndrewWUw <andrew_wu@outlook.com>
2014-05-14 15:42:26 +1000

    ~~~

--- ass4/VC/Scanner/SourceFile.java
+++ ass4/VC/Scanner/SourceFile.java
@@ -16,7 +16,7 @@ public class SourceFile {
 	public SourceFile(String filename) {
 		try {
 			reader = new LineNumberReader(new BufferedReader(new FileReader(
-					"VC/Parser/" + filename)));
+					"VC/Checker/" + filename)));
 		} catch (java.io.FileNotFoundException e) {
 			System.out.println("[# vc #]: can't read: " + filename);
 			System.exit(1);
