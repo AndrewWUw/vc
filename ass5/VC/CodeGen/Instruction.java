e19c751aa758f0c366813a1630d311a9c7cf360a
AndrewWUw <andrew_wu@outlook.com>
2014-05-29 14:17:52 +1000

    ~~~

--- ass5/VC/CodeGen/Instruction.java
+++ ass5/VC/CodeGen/Instruction.java
@@ -8,16 +8,16 @@ import java.io.PrintWriter;
 
 public class Instruction {
 
-  public String inst; // Jasmin assembly instruction
+	public String inst; // Jasmin assembly instruction
 
-  public Instruction (String inst) {
-    this.inst = inst;
-  }
-  
-  public void write(PrintWriter writer) {
-    if (! (inst.startsWith(".") || inst.endsWith(":")))
-      writer.print("\t");
-    writer.println(inst);
-  }
- 
-}  
+	public Instruction(String inst) {
+		this.inst = inst;
+	}
+
+	public void write(PrintWriter writer) {
+		if (!(inst.startsWith(".") || inst.endsWith(":")))
+			writer.print("\t");
+		writer.println(inst);
+	}
+
+}
